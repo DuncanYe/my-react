823f6232be22851a758c50908bca2a3f3e046fdd
Duncan <loxa10077@gmail.com>
2019-12-24 11:49:13 +0800

    show alert window when click number

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={function() { alert('出現彈跳視窗 !'); }}>
         {this.props.value}
       </button>
     );
